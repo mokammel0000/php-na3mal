5eb6db91404846dc6c3769a05a1aaea200258f11
Mohamed Mostafa <mokammel0000@gmail.com>
2024-05-08 18:47:59 +0300

    format some files

--- 06_Exceptions/index.php
+++ 06_Exceptions/index.php
@@ -30,47 +30,43 @@ function transfer($person, $amount, $dailyLim)
     ];*/
 
 
-    if($person->budget < $amount){
+    if ($person->budget < $amount) {
         throw new BalanceException('Low balance', 3003);
     }
 
-    if($person->dailyLimit < $dailyLim){
+    if ($person->dailyLimit < $dailyLim) {
         throw new LimitException('You have exceeded the limit', 502);
     }
     return 'Transfer Done';
-
 }
 
 
-try{
-    
-$ahmed = new stdClass();
-$ahmed->budget = 10000;
-$ahmed->dailyLimit = 500;
+try {
 
-$result = transfer($ahmed, 500, 30000);
-echo $result;
+    $ahmed = new stdClass();
+    $ahmed->budget = 10000;
+    $ahmed->dailyLimit = 500;
 
-}catch(BalanceException $e){
+    $result = transfer($ahmed, 500, 30000);
+    echo $result;
+} catch (BalanceException $e) {
     echo 'there is no enough money in your balance <br>';
-    echo $e->getMessage(). '<br>';
-    echo $e->getCode(). '<br>';
-    echo $e->getFile(). '<br>';
-    echo $e->getline(). '<br>';
-    echo $e->getTrace(). '<br>'; 
-              //if u have alot of files, and u don't know which of them is throwing the exeption
+    echo $e->getMessage() . '<br>';
+    echo $e->getCode() . '<br>';
+    echo $e->getFile() . '<br>';
+    echo $e->getline() . '<br>';
+    echo $e->getTrace() . '<br>';
+    //if u have alot of files, and u don't know which of them is throwing the exeption
 
-}catch(LimitException $e){
+} catch (LimitException $e) {
     echo 'you have excedeed the daily limit <br>';
-    echo $e->getMessage(). '<br>';
-    echo $e->getCode(). '<br>';
-    echo $e->getFile(). '<br>';
-    echo $e->getline(). '<br>';
-    echo $e->getTrace(). '<br>';
-
-}catch(Exception $e){
+    echo $e->getMessage() . '<br>';
+    echo $e->getCode() . '<br>';
+    echo $e->getFile() . '<br>';
+    echo $e->getline() . '<br>';
+    echo $e->getTrace() . '<br>';
+} catch (Exception $e) {
     echo 'other Exceptions <br>';
-    echo $e->getMessage(). '<br>';
+    echo $e->getMessage() . '<br>';
     echo $e->getCode();
-
 }
